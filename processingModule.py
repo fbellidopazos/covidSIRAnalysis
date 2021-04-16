39ee4aa98f69373281d82d0bb95ffe4af025ee33
fbellidopazos <fbellidopazos@gmail.com>
2021-04-17 00:11:24 +0200

    allCities Analysis

--- processingModule.py
+++ processingModule.py
@@ -1,7 +1,6 @@
 import csv
 
-dates = []
-infected = []
+allCities = {}
 
 with open('Data/datosCovid.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -12,15 +11,18 @@ with open('Data/datosCovid.csv') as csv_file:
             print(f'\t{row[1]} ------- {row[44]} ------- {row[27]}') # fecha infectados muertos --> titulo
             line_count += 1
         else:
-            if(row[3]=='Spain'):
-                if(row[43] == 'observed'): # para comprobar que los datos son observados y no proyecciones
-                    if(fecha==row[1]):
-                        print(f'HOLA')
-                        break
-                    fecha= row[1]
-                    print(f'\t{row[1]} ------- {round(float(row[44]))} ------- {round(float((row[27])))}') # fecha infectados muertos --> datos redondeados
-                    dates.append(row[1])
-                    infected.append(round(float(row[44])))
+            if(not row[3] in allCities):
+                allCities[row[3]] = []
+            
+            if(row[43] == 'observed'): # para comprobar que los datos son observados y no proyecciones
+                if(fecha==row[1]):
+                    print(f'FECHA REPETIDA')
+                    break
+                fecha= row[1]
+               #print(f'\t{row[1]} ------- {round(float(row[44]))} ------- {round(float((row[27])))}') # fecha infectados muertos --> datos redondeados
+                
+                par = (row[1],round(float(row[44])))
+                allCities[row[3]].append(par)
             line_count += 1
-print(dates)
-print(infected)
+#print(dates)
+print(allCities["Spain"])
